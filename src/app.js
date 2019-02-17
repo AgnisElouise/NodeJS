e776728b994442fdc5fe21fadc927da95f4ebac8
AgnisElouise <agniselouise@gmail.com>
2019-02-17 20:42:28 -0300

    criando um produto

--- src/app.js
+++ src/app.js
@@ -8,7 +8,10 @@ const app = express();
 const router = express.Router();
 
 //Conecta ao banco
-mongoose.connect('mongodb://agnis:agnis10@balta-shard-00-00-kwtej.azure.mongodb.net:27017,balta-shard-00-01-kwtej.azure.mongodb.net:27017,balta-shard-00-02-kwtej.azure.mongodb.net:27017/test?ssl=true&replicaSet=Balta-shard-0&authSource=admin&retryWrites=true')
+mongoose.connect('mongodb://agnis:agnis10@balta-shard-00-00-kwtej.azure.mongodb.net:27017,balta-shard-00-01-kwtej.azure.mongodb.net:27017,balta-shard-00-02-kwtej.azure.mongodb.net:27017/ndstr?ssl=true&replicaSet=Balta-shard-0&authSource=admin&retryWrites=true')
+
+//Carrega os Models
+const Product = require('./models/product');
 
 //Carrega as Rotas
 const indexRoute = require('./routes/index-route');
