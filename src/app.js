39bc8ffb7110df8282450f585e852e78963f66c4
AgnisElouise <agniselouise@gmail.com>
2019-02-17 20:08:38 -0300

    Configurando moongose

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//Conecta ao banco
+mongoose.connect('mongodb://agnis:agnis10@balta-shard-00-00-kwtej.azure.mongodb.net:27017,balta-shard-00-01-kwtej.azure.mongodb.net:27017,balta-shard-00-02-kwtej.azure.mongodb.net:27017/test?ssl=true&replicaSet=Balta-shard-0&authSource=admin&retryWrites=true')
 
 //Carrega as Rotas
 const indexRoute = require('./routes/index-route');
