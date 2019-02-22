2338fb5fd584430dc8b4d397a49187323e74b8cd
AgnisElouise <agniselouise@gmail.com>
2019-02-22 08:34:31 -0300

    arquivo de configuração

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
 const router = express.Router();
 
 //Conecta ao banco
-mongoose.connect('mongodb://agnis:agnis10@balta-shard-00-00-kwtej.azure.mongodb.net:27017,balta-shard-00-01-kwtej.azure.mongodb.net:27017,balta-shard-00-02-kwtej.azure.mongodb.net:27017/ndstr?ssl=true&replicaSet=Balta-shard-0&authSource=admin&retryWrites=true')
-
+mongoose.connect(config.connectionString);
 //Carrega os Models
 const Product = require('./models/product');
 const Customer = require('./models/customer');
---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SAL_KEY = 'f5b9942-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = '<strong>{0}</strong>';
+
+module.exports = {
+    connectionString: 'mongodb://agnis:agnis10@balta-shard-00-00-kwtej.azure.mongodb.net:27017,balta-shard-00-01-kwtej.azure.mongodb.net:27017,balta-shard-00-02-kwtej.azure.mongodb.net:27017/ndstr?ssl=true&replicaSet=Balta-shard-0&authSource=admin&retryWrites=true',
+    sendgridKey: 'TBD',
+    containerConnectionString: 'TBD'
+}
