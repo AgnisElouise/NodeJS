27c6cf6404198a324714d813b43b3dc3ec78621a
AgnisElouise <agniselouise@gmail.com>
2019-02-22 09:55:53 -0300

    enviando e-mail de boas vindas

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-global.SAL_KEY = 'f5b9942-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb://agnis:agnis10@balta-shard-00-00-kwtej.azure.mongodb.net:27017,balta-shard-00-01-kwtej.azure.mongodb.net:27017,balta-shard-00-02-kwtej.azure.mongodb.net:27017/ndstr?ssl=true&replicaSet=Balta-shard-0&authSource=admin&retryWrites=true',
-    sendgridKey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
