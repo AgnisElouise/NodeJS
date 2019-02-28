0e72d4683608ba23e31455cc13885f3ca03590bd
AgnisElouise <agniselouise@gmail.com>
2019-02-28 15:30:13 -0300

    upload da imagem do produto

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb://agnis:agnis10@balta-shard-00-00-kwtej.azure.mongodb.net:27017,balta-shard-00-01-kwtej.azure.mongodb.net:27017,balta-shard-00-02-kwtej.azure.mongodb.net:27017/ndstr?ssl=true&replicaSet=Balta-shard-0&authSource=admin&retryWrites=true',
-    sendgridKey: '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',
-    containerConnectionString: 'TBD'
+    sendgridKey: 'Chave ',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodestrbalta;AccountKey=rBk+S3ZuuvamXpgzdF/qB0ANh2ZfZKYkOubKf6N41a0wxRs8d7kfOqMV6Vwqf5dwEqZkbSHFkOkBmtkpcvyPBQ==;EndpointSuffix=core.windows.net'
 }
